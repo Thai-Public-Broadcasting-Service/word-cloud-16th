17985a158c5dffbc5ca569b5e5d687ec45cc6bb9
Tannysoft Macbook Pro <tannysoft@gmail.com>
2024-01-12 14:46:55 +0700

    change stucture

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TagCloud />
+  </div>
+</template>
+
+<script>
+import TagCloud from "./components/TagCloud";
+
+export default {
+  name: 'App',
+  components: {
+    TagCloud
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
